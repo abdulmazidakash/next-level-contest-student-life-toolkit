cd53fce7d0b1116447f103912a44688c6df32f1f
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-08-30 22:49:48 +0600

    frontend: successfully run client side vite app

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require('daisyui')],
+//   daisyui: {
+//     themes: [
+//       {
+//         toolkit: {
+//           "primary": "#317371",
+//           "secondary": "#A2B9A7",
+//           "accent": "#F67E04",
+//           "neutral": "#D9D9D9",
+//           "base-100": "#FFF9F1",
+//           "info": "#3ABFF8",
+//           "success": "#36D399",
+//           "warning": "#FBBD23",
+//           "error": "#F87272"
+//         }
+//       },
+//       "light"
+//     ]
+//   }
+// }
+
