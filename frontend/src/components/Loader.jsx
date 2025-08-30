5104404726b0a0a15a86df56421758dabbe8c249
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-08-31 00:05:09 +0600

    frontend: add loader

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+
+export default function Loader(){
+  return (
+    <div className="h-64 flex items-center justify-center">
+      <div className="text-center">
+        <div className="loader mb-2">⏳</div>
+        <div className="text-sm opacity-70">Loading...</div>
+      </div>
+    </div>
+  );
+}
+
